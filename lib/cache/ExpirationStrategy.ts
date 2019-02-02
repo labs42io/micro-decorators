3d05d7695049f1e1032597a45599b4952aaf48db
alex <new.nigol.42@gmail.com>
2019-02-02 16:01:04 +0200

    Add expiration strategy implementation

---
+++ lib/cache/ExpirationStrategy.ts
@@ -0,0 +1,39 @@
+import { StorageType } from './StorageType';
+
+export class ExpirationStrategy<K> {
+
+  private readonly expirations = new Map<K, NodeJS.Timeout>();
+
+  constructor(
+    private readonly storage: StorageType<K, unknown>,
+    private readonly expiration: 'absolute' | 'sliding',
+    private readonly timeout: number,
+  ) { }
+
+  public add(key: K): void {
+    this.addKey(key);
+  }
+
+  public update(key: K): void {
+    if (this.expiration !== 'sliding') {
+      return;
+    }
+
+    this.deleteKey(key);
+    this.addKey(key);
+  }
+
+  private addKey(key: K): void {
+    const timeout = setTimeout(
+      () => this.deleteKey(key),
+      this.timeout,
+    );
+    this.expirations.set(key, timeout);
+  }
+
+  private deleteKey(key: K): void {
+    this.expirations.delete(key);
+    this.storage.delete(key);
+  }
+
+}
