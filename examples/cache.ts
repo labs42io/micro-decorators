ab4dc581c1be04db8877c729321df948909a2c5b
alex <new.nigol.42@gmail.com>
2019-02-05 10:40:34 +0200

    cache example

--- examples/cache.ts
+++ examples/cache.ts
@@ -1,29 +1,23 @@
 import { cache } from '../lib/cache/';
 
-class A {
-  @cache(300, { scope: 'instance' })
-  public f(n: number): boolean {
-    for (let i = 2; i <= Math.sqrt(n); i += 1) {
-      if (n % i === 0) return false;
-    }
-    return true;
+class Service {
+  @cache(1000, { scope: 'class' })
+  public f(n: number): Promise<number> {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(n ** 2), 10);
+    });
   }
-}
 
-class B {
-
-  @cache(400, { scope: 'class' })
+  @cache(1000, { scope: 'class' })
   public g(n: number): number {
     return n ** 2;
   }
 }
 
-console.log('a');
-const a = new A();
-console.log('b');
-const b = new A();
-console.log(a, b);
+const s = new Service();
+console.log(s.f(4));
+console.log(s.g(4));
 
-a.f(3);
-b.f(4);
-new B().g(5);
+new Service().f(3); // first call => no cached
+new Service().f(3); // second call => cached
+new Service().f(4); // first call with this arguments => no cached
