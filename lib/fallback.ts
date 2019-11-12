a5fefb470d6b8163ad796cb99c4ed9fe61a34f74
Artiom <artiom.popcov@labs42.io>
2019-11-12 11:53:58 +0200

    Fallback

--- lib/fallback.ts
+++ lib/fallback.ts
@@ -10,7 +10,74 @@ export type FallbackOptions = {
  * Allows to provide a substitute value when method call fails or rejects.
  * @param options optional fallback options
  */
-export function fallback(valueProvider: (...args: any[]) => any, options?: FallbackOptions);
-export function fallback(value: any, options?: FallbackOptions) {
-  throw new Error('Not implemented.');
+export function fallback(valueProvider: (...args: any[]) => any, options?: FallbackOptions): any;
+export function fallback(value: any, options?: FallbackOptions): any;
+export function fallback(value: any | ((...args: any[]) => any), options?: FallbackOptions): any {
+
+  return function (target: any, propertyKey: any, descriptor: PropertyDescriptor) {
+    const method = descriptor.value;
+
+    descriptor.value = function () {
+      try {
+        const result = method.call(this, arguments);
+        const isPromiseLike = result instanceof Promise;
+
+        if (isPromiseLike) {
+          return fallbackPromise(options, value, result, this);
+        }
+
+        return result;
+
+      } catch (err) {
+        const isFiltered = filterError(err, options);
+
+        if (isFiltered) {
+          return getFallbackValue(value, this);
+        }
+
+        throw new Error(err.message);
+      }
+    };
+
+  };
+}
+
+function fallbackPromise(
+  options: FallbackOptions,
+  value: any,
+  result: any,
+  instance: any,
+): Promise<any> {
+
+  const resolve = (response: any) => {
+    return Promise.resolve(response);
+  };
+
+  const reject = (err: any) => {
+    const isFiltered = filterError(err, options);
+
+    return isFiltered
+      ? Promise.resolve(getFallbackValue(value, instance))
+      : Promise.reject(err);
+  };
+
+  return Promise.resolve(result).then(resolve, reject);
+}
+
+function filterError(error: Error, options: FallbackOptions): boolean {
+  if (!options || !options.errorFilter) {
+    return true;
+  }
+
+  return options.errorFilter(error);
+}
+
+function getFallbackValue(value: any | ((...args: any[]) => any), instance: any): any {
+  const isFunction = typeof value === 'function';
+
+  if (isFunction) {
+    return value.bind(instance)();
+  }
+
+  return value;
 }
---
+++ test/fallback.spec.ts
@@ -0,0 +1,152 @@
+import { fallback } from '../lib/fallback';
+import { expect } from 'chai';
+
+describe('@fallback', () => {
+
+  describe('synchronous method', () => {
+
+    it('should take the real value', () => {
+      class TestClass {
+        @fallback(42)
+        test() {
+          return 4242;
+        }
+      }
+
+      const target = new TestClass();
+      const result = target.test();
+
+      expect(result).to.equal(4242);
+    });
+
+    it('should take the fallback value', () => {
+      class TestClass {
+        @fallback(42)
+        test() {
+          throw new Error('4242');
+        }
+      }
+
+      const target = new TestClass();
+      const result = target.test();
+
+      expect(result).to.equal(42);
+    });
+
+    it('should take the fallback provided value', () => {
+      class TestClass {
+        value = 42;
+        @fallback(function () { return this.value; })
+        test() {
+          throw new Error('4242');
+        }
+      }
+
+      const target = new TestClass();
+      const result = target.test();
+
+      expect(result).to.equal(42);
+    });
+
+    it('should return fallback by filtered error', () => {
+      class TestClass {
+        @fallback(42, { errorFilter: ({ message }: Error) => message === '4242' })
+        test() {
+          throw new Error('4242');
+        }
+      }
+
+      const target = new TestClass();
+      const result = target.test();
+
+      expect(result).to.equal(42);
+    });
+
+    it('should throw error', () => {
+      class TestClass {
+        @fallback(42, { errorFilter: ({ message }: Error) => message === '3131' })
+        test() {
+          throw new Error('4242');
+        }
+      }
+
+      const target = new TestClass();
+      expect(target.test).to.throw(Error);
+    });
+
+  });
+
+  describe('asynchronous method', () => {
+
+    it('should take the real value', async () => {
+      class TestClass {
+        @fallback(42)
+        test() {
+          return new Promise(resolve => resolve(4242));
+        }
+      }
+
+      const target = new TestClass();
+      const result = await target.test();
+
+      expect(result).to.equal(4242);
+    });
+
+    it('should take the fallback value', async () => {
+      class TestClass {
+        @fallback(42)
+        test() {
+          return new Promise((_, reject) => reject(new Error('4242')));
+        }
+      }
+
+      const target = new TestClass();
+      const result = await target.test();
+
+      expect(result).to.equal(42);
+    });
+
+    it('should take the fallback provided value', async () => {
+      class TestClass {
+        value = 42;
+        @fallback(function () { return this.value; })
+        test() {
+          return new Promise((_, reject) => reject(new Error('4242')));
+        }
+      }
+
+      const target = new TestClass();
+      const result = await target.test();
+
+      expect(result).to.equal(42);
+    });
+
+    it('should return fallback by filtered error', async () => {
+      class TestClass {
+        @fallback(42, { errorFilter: ({ message }: Error) => message === '4242' })
+        test() {
+          return new Promise((_, reject) => reject(new Error('4242')));
+        }
+      }
+
+      const target = new TestClass();
+      const result = await target.test();
+
+      expect(result).to.equal(42);
+    });
+
+    it('should reject an error', async () => {
+      class TestClass {
+        @fallback(42, { errorFilter: ({ message }: Error) => message === '3131' })
+        test() {
+          return new Promise((_, reject) => reject(new Error('4242')));
+        }
+      }
+
+      const target = new TestClass();
+      await expect(target.test()).to.be.rejectedWith('4242');
+    });
+
+  });
+
+});
